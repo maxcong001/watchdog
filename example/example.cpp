2d676e6409e23a722a91129fd2da2d5a7be3ecaf
savagecm <savagecm@qq.com>
2018-10-13 09:06:38 +0800

    aa

--- example/example.cpp
+++ example/example.cpp
@@ -6,10 +6,40 @@
 #include <boost/asio/ip/address.hpp>
 #include <boost/format.hpp>
 #include <boost/asio/deadline_timer.hpp>
+#include <thread>
+#include <chrono>
+
+boost::asio::io_service ioService;
+
+boost::asio::deadline_timer t(ioService, boost::posix_time::seconds(1));
+
+void print(const boost::system::error_code & /*e*/)
+{
+    std::cout << "Hello, world!\n";
+    t.expires_from_now(boost::posix_time::seconds(1));
+    t.async_wait(print);
+}
+
 int main()
 {
     set_log_level(logger_iface::log_level::debug);
-    boost::asio::io_service ioService;
 
-    watchDog wd_test()
+    std::thread asio_thread([&]() {
+        __LOG(debug, "ioservice is running");
+        ioService.run();
+    });
+    t.async_wait(print);
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    watchDog wd_test(ioService);
+    wd_test.start(2, 2, []() {
+        __LOG(debug, "heart beat lost");
+    });
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    __LOG(debug, "now peg watch dog");
+    wd_test.peg();
+    std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+    ioService.stop();
+    asio_thread.join();
+    __LOG(debug, "exiting example");
 }
--- include/watchDog/watchDog.hpp
+++ include/watchDog/watchDog.hpp
@@ -2,6 +2,9 @@
 #include <functional>
 #include <boost/asio.hpp>
 #include <boost/asio/deadline_timer.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/bind.hpp>
+#include "logger/logger.hpp"
 class watchDog
 {
   public:
@@ -10,42 +13,60 @@ class watchDog
     }
     ~watchDog()
     {
+        __LOG(debug, "dtor of watch dog");
     }
     bool start(int interval, int times, std::function<void()> cb_function)
     {
+        __LOG(debug, "start watch dog, interval is :" << interval << ", times is :" << times);
         _max_lost_times = times;
         _on_lost = cb_function;
-        _peg_timer.expires_from_now(boost::posix_time::seconds(interval));
-        _peg_timer.async_wait([&](const boost::system::error_code &ec) {
-            if (!ec)
+        _interval = interval;
+        _peg_timer.expires_from_now(boost::posix_time::seconds(_interval));
+        _peg_timer.async_wait(boost::bind(&watchDog::handle_timer, this, _1));
+        return true;
+    }
+
+    void handle_timer(const boost::system::error_code &ec)
+    {
+        if (!ec)
+        {
+            _lost_times++;
+            if (_lost_times >= _max_lost_times)
             {
-                _lost_times++;
-                if (_lost_times >= _max_lost_times)
-                {
-                    _on_lost();
-                }
+                _on_lost();
+                _lost_times = 0;
             }
-            else
-            {
-                __LOG(error, "timer error");
-            }
-        });
+            __LOG(debug, "peg timer expire, now lost time is :" << _lost_times);
+            _peg_timer.expires_from_now(boost::posix_time::seconds(_interval));
+            _peg_timer.async_wait(boost::bind(&watchDog::handle_timer, this, _1));
+        }
+        else
+        {
+            __LOG(error, "timer error");
+        }
     }
     bool stop()
     {
+        __LOG(debug, "stop watch dog");
         _peg_timer.cancel();
         _lost_times = 0;
+        return true;
     }
     bool peg()
     {
+        __LOG(debug, "peg watch dog");
         _lost_times = 0;
+
         _peg_timer.expires_from_now(boost::posix_time::seconds(_interval));
+        _peg_timer.async_wait(boost::bind(&watchDog::handle_timer, this, _1));
+
+        return true;
     }
 
     boost::asio::io_service &_io_service;
-    deadline_timer _peg_timer;
-    int _max_lost_times;
+    boost::asio::deadline_timer _peg_timer;
     int _lost_times;
     int _interval;
     std::function<void()> _on_lost;
+    int _max_lost_times;
 };
